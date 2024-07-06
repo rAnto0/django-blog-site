e211805ec5509adf89d089ba5f0cc5645ae3af95
rAnto <dark_neo3000@mail.ru>
2024-07-06 11:39:15 +0300

    pagination

--- mydjangoproject/main_app/views.py
+++ mydjangoproject/main_app/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
@@ -9,10 +10,17 @@ from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import PostsSerializer
 
 
+class PostsAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class PostsAPIList(generics.ListCreateAPIView):
     queryset = Posts.objects.all()
     serializer_class = PostsSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = PostsAPIListPagination
 
 
 class PostsAPIUpdate(generics.RetrieveUpdateAPIView):
